a8d034c797c3788dac2d0595d4764fc30612c5d6
Henry Schreiner <henryschreineriii@gmail.com>
2020-12-04 00:16:12 -0500

    feat: people mode

--- ghimpact/d3.py
+++ ghimpact/d3.py
@@ -4,8 +4,9 @@ import sys
 import itertools
 import json
 from pathlib import Path
+from typing import List, Dict, Any
 
-import hist
+import hist  # type: ignore
 import yaml
 import click
 
@@ -14,7 +15,8 @@ import click
 @click.argument("input_files", type=click.File("r"), nargs=-1)
 @click.option("--options", type=click.File("r"))
 @click.option("--output", type=click.File("w"), default="-")
-def d3(input_files, options, output):
+@click.option("--people/--no-people", default=False)
+def d3(input_files: List[Any], options: Any, output: Any, people: bool) -> None:
 
     opts = yaml.safe_load(options)
     cats = opts["categories"]
@@ -37,27 +39,37 @@ def d3(input_files, options, output):
         org = (pr["repository_url"].split("/")[-2] for pr in data)
         repo = (pr["repository_url"].split("/")[-1] for pr in data)
 
-        org_repo = [f"{o}:{r}" for o, r in zip(org, repo) if o in all_orgs and (not filter_repos or r in filter_repos)]
+        org_repo = [
+            f"{o}:{r}"
+            for o, r in zip(org, repo)
+            if o in all_orgs and (not filter_repos or r in filter_repos)
+        ]
 
         h.fill(
             author=author_file.stem,
             org_repo=org_repo,
         )
 
-    nodes = get_nodes(h)
+    nodes = get_nodes(h, cats)
     print(f"Built nodes list with {len(nodes)} nodes")
 
-    links = get_people_links(h, nodes)
-    print("Filled links histogram")
+    if not people:
+        links = get_people_links(h, nodes)
+        print("Filled links histogram")
+        out_dict = dict(nodes=nodes, links=links)
 
-    out_dict = dict(nodes=nodes, links=links)
+    else:
+        people_nodes = get_people_nodes(h)
+        links = repo_people_links(h)
+        out_dict = dict(nodes=nodes + people_nodes, links=links)
+
     j = json.dumps(out_dict, sort_keys=True, indent=2)
     print(j, file=output)
 
     print("Finished")
 
 
-def nodes(h: hist.Hist) -> List[str]:
+def get_nodes(h: hist.Hist, cats: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     nodes = []
     for org_repo in h.axes["org_repo"]:
         org = org_repo.split(":")[0]
@@ -66,15 +78,27 @@ def nodes(h: hist.Hist) -> List[str]:
                 group = cat["value"]
                 break
         else:
-             raise ValueError(f"That can't happen, {org} is already filtered!")
+            raise ValueError(f"That can't happen, {org} is already filtered!")
 
         nodes.append(dict(id=org_repo, group=group))
 
     return nodes
 
 
+def get_people_nodes(h: hist.Hist) -> List[Dict[str, int]]:
+    return [{"id": a, "group": 0} for a in h.axes["author"]]
 
-def get_people_links(h: hist.Hist, nodes: List[str]) -> List[str]:
+
+def repo_people_links(h: hist.Hist):
+    return [
+        dict(source=author, target=org_repo, value=value)
+        for author in h.axes["author"]
+        for org_repo in h.axes["org_repo"]
+        if (value := h[author, org_repo]) > 0
+    ]
+
+
+def get_people_links(h: hist.Hist, nodes: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     nodes_ids = [v["id"] for v in nodes]
 
     lh = hist.Hist(
@@ -88,14 +112,12 @@ def get_people_links(h: hist.Hist, nodes: List[str]) -> List[str]:
             if (res1 := h[author, a]) > 0 and (res2 := h[author, b] > 0):
                 lh.fill(source=a, target=b)
 
-    for source in lh.axes["source"]:
-        for target in lh.axes["target"]:
-            if (value := lh[source, target]) > 0:
-                links.append(dict(source=source, target=target, value=value))
-
-    return links
-
-
+    return [
+        dict(source=source, target=target, value=value)
+        for source in lh.axes["source"]
+        for target in lh.axes["target"]
+        if (value := lh[source, target]) > 0
+    ]
 
 
 if __name__ == "__main__":
